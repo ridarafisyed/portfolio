920a89e15e0070d7539082f2151fbcc608dfd12f
Rida R Syed <ridarafisyed@yahoo.com>
2020-06-11 22:28:22 +0500

    update links

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { NavigationBar } from './components/static_pages/NavigationBar';
 import { Home } from './components/static_pages/Home';
 import { Contact } from './components/static_pages/Contact';
-import { NoMatch } from './components/static_pages/NoMatch';
-
 import {ExpenseTracker} from './components/static_pages/ExpenseTracker';
-import { Covid19 } from './components/static_pages/Covid19';
+import {Covid19} from './components/static_pages/Covid19';
 
 
 import './App.css';
@@ -23,7 +21,6 @@ function App() {
           <Route path="/contact" component={Contact} />
           <Route path="/expense" component={ExpenseTracker} />
           <Route path="/covid" component={Covid19} />
-          <Route component={NoMatch} />
         </Switch>
         </div>
       </Router>
--- src/components/static_pages/Home.js
+++ src/components/static_pages/Home.js
@@ -1,24 +1,16 @@
 import React from 'react';
 
-//import styled from 'styled-components';
+
 import './StaticPage.css'
 import './General.css'
-// const GridWrapper = styled.div`
-//   display: grid;
-//   grid-gap: 10px;
-//   margin-top: 1em;
-//   margin-left: 6em;
-//   margin-right: 6em;
-//   grid-template-columns: repeat(12, 1fr);
-//   grid-auto-rows: minmax(25px, auto);
-// `;
+
 
 export const Home = (props) => (
   
   <div className='container_glass '>
     <h1 className="display-3">Welcome</h1>
-     <button type="button" className="btn btn-danger btn-lg" href='/covid19'><i className='fa fa-heartbeat'></i> COVID-19 Trend </button>
+     <a type="button" className="btn btn-danger btn-lg" href='/covid19'><i className='fa fa-heartbeat'></i> COVID-19 Trend </a>
      <span>  </span>
-    <button type="button" className="btn btn-success btn-lg" href='/expense'> <i className='fas fa-calculator'></i> Expense Tracker</button>
+    <a type="button" className="btn btn-success btn-lg" href='/expense'> <i className='fas fa-calculator'></i> Expense Tracker</a>
   </div>
 )
