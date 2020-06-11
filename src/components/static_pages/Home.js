202cf01e3134f4372ac2af969c14efea79e83868
Rida R Syed <ridarafisyed@yahoo.com>
2020-06-11 22:13:58 +0500

    changing in website
    changing in styling

--- src/components/static_pages/Home.js
+++ src/components/static_pages/Home.js
@@ -16,6 +16,9 @@ import './General.css'
 export const Home = (props) => (
   
   <div className='container_glass '>
-    <h1 className="display-4">Welcome to My website</h1>
+    <h1 className="display-3">Welcome</h1>
+     <button type="button" className="btn btn-danger btn-lg" href='/covid19'><i className='fa fa-heartbeat'></i> COVID-19 Trend </button>
+     <span>  </span>
+    <button type="button" className="btn btn-success btn-lg" href='/expense'> <i className='fas fa-calculator'></i> Expense Tracker</button>
   </div>
 )
