7ccc8e6fea087fba92001246b5b664aee68cdffd
Rida R Syed <ridarafisyed@yahoo.com>
2020-06-11 22:32:14 +0500

    Update Home.js

--- src/components/static_pages/Home.js
+++ src/components/static_pages/Home.js
@@ -8,7 +8,7 @@ import './General.css'
 export const Home = (props) => (
   
   <div className='container_glass '>
-    <h1 className="display-3">Welcome</h1>
+    <h1 className="display-3"></h1>
      <a type="button" className="btn btn-danger btn-lg" href='/covid19'><i className='fa fa-heartbeat'></i> COVID-19 Trend </a>
      <span>  </span>
     <a type="button" className="btn btn-success btn-lg" href='/expense'> <i className='fas fa-calculator'></i> Expense Tracker</a>
